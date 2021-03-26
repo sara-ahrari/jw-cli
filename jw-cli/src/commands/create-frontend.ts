1a992cdbc5bebb9fc941c22708590ab7e73357a5
Tselmeg Baasan <ts2530ba-s@student.lu.se>
2021-03-26 10:40:24 +0100

    Refactoring

--- jw-cli/src/commands/create-frontend.ts
+++ jw-cli/src/commands/create-frontend.ts
@@ -34,84 +34,111 @@ export default class CreateFrontend extends Command {
     let includeFormatting: boolean;
     let lintingStyle: string = '';
     let includeGitHooks: boolean;
-    let reactCommand: string = ''
 
     //First round of questions
-    let responses: any = await prompt([
+    const basicConfiguration: any = await prompt([
       {
         type: 'input',
         name: 'projectName',
-        message: 'What is the name of your React project?',
+        message: 'What is the name of your React project? (lower case only)',
         required: true,
+        validate: (answer) => answer === answer.toLowerCase()
       },
       {
         type: 'list',
         name: 'programmingLanguage',
         message: 'Please select the language you would like to use in your React project: ',
-        required: true,
-        choices: ['Javascript', 'Typescript'],
-      },
+        choices: ['Javascript', 'Typescript']
+      }
+    ])
+
+    projectName = basicConfiguration.projectName
+    programmingLanguage = basicConfiguration.programmingLanguage
+
+    const reduxConfiguration: any = await prompt([
       {
         type: 'list',
         name: 'reduxType',
         message: 'Would you like to include Redux for state handling?',
-        required: true,
         choices: ['No', 'Redux', 'Redux Toolkit'],
+      },
+      {
+        type: 'input',
+        name: 'reduxVersion',
+        message: (answers) => `Which version of ${answers.reduxType} would you like?:  `,
+        when: (answers) => answers.reduxType !== 'No',
+        default: 'latest',
+        validate: (answer) => (answer.split('.').length === 3 && !answer.split('.').includes(' ')) || answer === 'latest'
       }
     ])
 
-    //Saving inputs from first round of prompts
-    projectName = responses.projectName;
-    programmingLanguage = responses.programmingLanguage;
-    reduxType = responses.reduxType;
+    reduxType = reduxConfiguration.reduxType
+    reduxVersion = reduxConfiguration.reduxVersion
 
-    //If user wants to include redux, prompt the following and determine version
-    if (reduxType !== 'No') reduxVersion = await versionInputMethod(reduxType)
-
-    includeStyledComponents = await includeTech('styled-components')
-    if (includeStyledComponents) styledComponentsVersion = await versionInputMethod('styled-components')
-
-    responses = await prompt([{
-      type: 'list',
-      name: 'lintingOrFormatting',
-      message: 'Please select option for linting and formatting: ',
-      choices: ['Eslint + Prettier', 'Eslint', 'Prettier', 'None']
-    }])
-
-    let lintingOrFormatting: string = responses.lintingOrFormatting.split(' + ')
-
-    includeLinting = lintingOrFormatting.includes('Eslint')
-    includeFormatting = lintingOrFormatting.includes('Prettier')
-
-    if (includeLinting) {
-      responses = await prompt([{
+    const lintingFormattingConfiguration: any = await prompt([
+      {
+        type: 'list',
+        name: 'lintingFormatting',
+        message: 'Please select option for linting and formatting:',
+        choices: ['Eslint + Prettier', 'Eslint', 'Prettier', 'None']
+      },
+      {
         type: 'list',
         name: 'eslintStyleGuide',
         message: 'Please select a style guide for Eslint: ',
-        choices: ['standard', 'airbnb', 'google']
-      }])
+        choices: ['standard', 'airbnb', 'google'],
+        when: (answers) => answers.lintingFormatting.includes('Eslint')
+      },
+      {
+        type: 'confirm',
+        name: 'husky',
+        message: 'Would you like to include pre-commits (husky.js) to your project?',
+        when: (answers) => answers.lingingFormatting !== 'None'
+      }
+    ])
 
-      lintingStyle = responses.eslintStyleGuide
-    }
+    includeLinting = lintingFormattingConfiguration.lintingFormatting.includes('Eslint')
+    includeFormatting = lintingFormattingConfiguration.lintingFormatting.includes('Prettier')
+    lintingStyle = lintingFormattingConfiguration.eslintStyleGuide
+    includeGitHooks = lintingFormattingConfiguration.husky
 
+    const stylingConfiguration: any = await prompt([
+      {
+        type: 'confirm',
+        name: 'includeStyledComponents',
+        message: 'Would you like to include styled-components to your project?'
+      },
+      {
+        type: 'input',
+        name: 'styledComponentsVersion',
+        message: 'Which version of styledComponents would you like?:  ',
+        when: (answers) => answers.includeStyledComponents,
+        default: 'latest',
+        validate: (answer) => (answer.split('.').length === 3 && !answer.split('.').includes(' ')) || answer === 'latest'
+      }
+    ])
 
-    includeGitHooks = includeLinting || includeFormatting ? await includeTech('pre-commit hooks (Husky.js)') : false
+    includeStyledComponents = stylingConfiguration.includeStyledComponents
+    styledComponentsVersion = stylingConfiguration.styledComponentsVersion
 
     //Important paths
     const projectPath: string = process.cwd();
     const insideProjectPath: string = `${process.cwd()}/${projectName}`;
-    let installCommand: string = ''
+    
 
     //Determine create-react-app command 
+    let installReduxCommand: string = ''
+    let reactCommand: string = ''
+
     switch (reduxType) {
       case 'Redux': {
         reactCommand = programmingLanguage === 'Typescript' ? `npx create-react-app ${projectName} --template typescript` : `npx create-react-app ${projectName}`;
-        installCommand = `npm install react-redux${reduxVersion}`
+        installReduxCommand = `npm install react-redux@${reduxVersion}`
         break;
       }
 
       case 'Redux Toolkit': {
-        reactCommand = programmingLanguage === 'Typescript' ? `npx create-react-app ${projectName} --template redux-typescript${reduxVersion}` : `npx create-react-app ${projectName} --template redux${reduxVersion}`;
+        reactCommand = programmingLanguage === 'Typescript' ? `npx create-react-app ${projectName} --template redux-typescript@${reduxVersion}` : `npx create-react-app ${projectName} --template redux@${reduxVersion}`;
         break;
       }
 
@@ -125,48 +152,41 @@ export default class CreateFrontend extends Command {
     //Run create-react-app with config
     await executeShellCommand(reactCommand, projectPath)
 
-    installCommand !== '' && await executeShellCommand(installCommand, insideProjectPath)
+    installReduxCommand !== '' && await executeShellCommand(installReduxCommand, insideProjectPath)
 
     if (includeStyledComponents) {
-      installCommand = `npm install --save styled-components${styledComponentsVersion}`;
-      await executeShellCommand(installCommand, insideProjectPath)
+      const cmd = `npm install --save styled-components@${styledComponentsVersion}`;
+      await executeShellCommand(cmd, insideProjectPath)
     }
 
-   
-
-    const lintingStyleDict: {[key: string]: (param: boolean) => void;} = {
+    const lintingStyleDict: { [key: string]: (param: boolean) => void; } = {
       standard: standardSetup,
       airbnb: airbnbSetup,
       google: googleSetup
     }
 
-    if(includeLinting){
+    if (includeLinting) {
       lintingStyleDict[lintingStyle](includeFormatting);
     } else {
-      if(includeFormatting){
+      if (includeFormatting) {
         prettierSetupOnly();
       }
     }
 
     if (includeGitHooks) {
-      installCommand = `npm install --save-dev lint-staged husky@4.3.8 prettier`;
-      await executeShellCommand(installCommand, insideProjectPath)
-
-      const touchCommand: string = `touch .prettierignore .prettierrc`;
-      await executeShellCommand(touchCommand, insideProjectPath)
-
+      const cmd = `npm install --save-dev lint-staged husky@4.3.8`;
+      await executeShellCommand(cmd, insideProjectPath)
       await editJsonFile(`${insideProjectPath}/package.json`, ["husky", "lint-staged"]);
     }
   }
 }
 
-// --- HELPER METHODS ---
-//Helper method for getting versions for various technologies
+// --- SETUP METHODS ---
+
 const standardSetup = (formatting: boolean) => {
-  //Function for setting up eslint standard + prettier
+  //Function for setting up eslint standard + prettiero
   formatting ? console.log('Setting up Eslint Standard + Prettier') : console.log('Setting up Eslint Standard')
 }
-
 
 const airbnbSetup = (formatting: boolean) => {
   //Function for setting up eslint airbnb + prettier
@@ -180,32 +200,12 @@ const googleSetup = (formatting: boolean) => {
 }
 
 const prettierSetupOnly = () => {
-  //Function for setting up prettier only
-  console.log('Setting up Prettier only')
+  /* 
+  1. install prettier 
+  2. 
+  
+  
+  */
+  
 }
 
-const versionInputMethod = async (technology: string) => {
-  let input: any = await prompt([
-    {
-      type: 'input',
-      name: 'version',
-      message: `What version of ${technology} would you like? (Leave blank for latest version)`
-    }
-  ])
-
-  let res = input.version === '' ? '' : `@${input.version}`
-  return res
-}
-
-//Helper method for checking if user wants to include a specific technology
-const includeTech = async (technology: string) => {
-  let input: any = await prompt([
-    {
-      type: 'confirm',
-      name: 'include',
-      message: `Would you like to include ${technology} in your project?`,
-      default: true
-    }
-  ])
-  return input.include;
-}
