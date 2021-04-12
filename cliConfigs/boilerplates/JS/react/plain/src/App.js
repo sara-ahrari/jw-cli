846ba76f5db30b335dfb26be65b1d5167bf652d9
Tselmeg Baasan <ts2530ba-s@student.lu.se>
2021-04-12 15:17:33 +0200

    logic added for creating react apps

---
+++ cliConfigs/boilerplates/JS/react/plain/src/App.js
@@ -0,0 +1,36 @@
+import { ReactComponent as Logo } from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo style={{ height: '30vmin' }} />
+        <p>
+          Hurray, you have just created a React app with Jayway project template
+          CLI!
+        </p>
+        <div className="link-container">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn more about React
+          </a>
+          <a
+            className="App-link"
+            href="https://www.npmjs.com/package/jw-project-template-cli"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read more about this CLI
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
--- src/commands/create-frontend.ts
+++ src/commands/create-frontend.ts
@@ -1,13 +1,13 @@
 import { Command, flags } from '@oclif/command';
 import {
   executeShellCommand,
-  // editJsonFile,
-  // copyProjectConfigFile,
-  // generateEslintConfig,
+  editJsonFile,
+  copyProjectConfigFile,
+  generateEslintConfig,
   copyFolder,
 } from '../utils';
 import * as prompts from '../promts';
-// import cli from 'cli-ux';
+import cli from 'cli-ux';
 
 export default class CreateFrontend extends Command {
   static description =
@@ -21,38 +21,39 @@ export default class CreateFrontend extends Command {
     const basicConfig: any = await prompts.basicConfiguration();
     const projectName: string = basicConfig.projectName;
     const programmingLanguage: string = basicConfig.programmingLanguage;
-    // const langString: string =
-    //   programmingLanguage === 'Javascript' ? 'JS' : 'TS';
+    const langString: string =
+      programmingLanguage === 'Javascript' ? 'JS' : 'TS';
 
-    // const reduxConfig: any = await prompts.reduxConfiguration();
-    // const reduxType: string = reduxConfig.reduxType;
-    // const reduxVersion: string = reduxConfig.reduxVersion;
+    const reduxConfig: any = await prompts.reduxConfiguration();
+    const reduxType: string = reduxConfig.reduxType;
+    const reduxVersion: string = reduxConfig.reduxVersion;
 
-    // const lintingConfig: any = await prompts.lintingFormattingConfiguration();
-    // const includeLinting: boolean = lintingConfig.lintingFormatting.includes(
-    //   'Eslint'
-    // );
-    // const includeFormatting: boolean = lintingConfig.lintingFormatting.includes(
-    //   'Prettier'
-    // );
-    // const lintingStyle: string = lintingConfig.eslintStyleGuide;
-    // const includeGitHooks: boolean = lintingConfig.husky;
+    const lintingConfig: any = await prompts.lintingFormattingConfiguration();
+    const includeLinting: boolean = lintingConfig.lintingFormatting.includes(
+      'Eslint'
+    );
+    const includeFormatting: boolean = lintingConfig.lintingFormatting.includes(
+      'Prettier'
+    );
+    const lintingStyle: string = lintingConfig.eslintStyleGuide;
+    const includeGitHooks: boolean = lintingConfig.husky;
 
-    // const styledComponentsConfig: any = await prompts.stylingConfiguration();
-    // const includeStyledComponents: boolean =
-    //   styledComponentsConfig.includeStyledComponents;
-    // const styledComponentsVersion: string =
-    //   styledComponentsConfig.styledComponentsVersion;
+    const styledComponentsConfig: any = await prompts.stylingConfiguration();
+    const includeStyledComponents: boolean =
+      styledComponentsConfig.includeStyledComponents;
+    const styledComponentsVersion: string =
+      styledComponentsConfig.styledComponentsVersion;
 
-    // const documentationConfig: any = await prompts.documentationConfiguration();
-    // const includeDocumentation: boolean =
-    //   documentationConfig.includeDocumentation;
+    const documentationConfig: any = await prompts.documentationConfiguration();
+    const includeDocumentation: boolean =
+      documentationConfig.includeDocumentation;
 
     /* Important paths */
     const projectPath: string = process.cwd();
     const insideProjectPath = `${process.cwd()}/${projectName}`;
 
     const setupReact = async () => {
+      cli.action.start('Installing React...');
       const cmd =
         programmingLanguage === 'Typescript'
           ? `npx create-react-app ${projectName} --template typescript`
@@ -60,168 +61,178 @@ export default class CreateFrontend extends Command {
       await executeShellCommand(cmd, projectPath);
       const removeFoldersCmd = 'rm -rf src public';
       await executeShellCommand(removeFoldersCmd, insideProjectPath);
-      copyFolder('boilerplates/reactJS', insideProjectPath);
+      cli.action.stop();
+
+      if (includeStyledComponents) {
+        cli.action.start('Installing styled components...');
+        const styledComponentCmd = `npm install --save styled-components@${styledComponentsVersion}`;
+        await executeShellCommand(styledComponentCmd, insideProjectPath);
+        copyFolder(
+          `boilerplates/${langString}/react/styledcomponent`,
+          insideProjectPath
+        );
+        cli.action.stop();
+      } else {
+        copyFolder(`boilerplates/${langString}/react/plain`, insideProjectPath);
+      }
     };
 
-    setupReact();
+    const setupReactRedux = async () => {
+      cli.action.start('Installing React with Redux Toolkit.. ');
+      const cmd =
+        programmingLanguage === 'Typescript'
+          ? `npx create-react-app ${projectName} --template redux-typescript@${reduxVersion}`
+          : `npx create-react-app ${projectName} --template redux@${reduxVersion}`;
 
-    /* Determine create-react-app command */
-    // let installReduxCommand = '';
-    // let reactCommand = '';
+      await executeShellCommand(cmd, projectPath);
+      const removeFoldersCmd = 'rm -rf src public';
+      await executeShellCommand(removeFoldersCmd, insideProjectPath);
+      cli.action.stop();
 
-    // switch (reduxType) {
-    //   case 'Redux': {
-    //     reactCommand =
-    //       programmingLanguage === 'Typescript'
-    //         ? `npx create-react-app ${projectName} --template typescript`
-    //         : `npx create-react-app ${projectName}`;
-    //     installReduxCommand = `npm install react-redux@${reduxVersion}`;
-    //     break;
-    //   }
+      if (includeStyledComponents) {
+        cli.action.start('Installing styled components...');
+        const styledComponentCmd = `npm install --save styled-components@${styledComponentsVersion}`;
+        await executeShellCommand(styledComponentCmd, insideProjectPath);
+        copyFolder(
+          `boilerplates/${langString}/redux/styledcomponent`,
+          insideProjectPath
+        );
+        cli.action.stop();
+      } else {
+        copyFolder(`boilerplates/${langString}/redux/plain`, insideProjectPath);
+      }
+    };
 
-    //   case 'Redux Toolkit': {
-    //     reactCommand =
-    //       programmingLanguage === 'Typescript'
-    //         ? `npx create-react-app ${projectName} --template redux-typescript@${reduxVersion}`
-    //         : `npx create-react-app ${projectName} --template redux@${reduxVersion}`;
-    //     break;
-    //   }
+    switch (reduxType) {
+      case 'Redux': {
+        cli.action.start('Installing React with Redux.. ');
+        await setupReact();
+        const npmCmd = `npm install react-redux@${reduxVersion}`;
+        await executeShellCommand(npmCmd, insideProjectPath);
+        cli.action.stop();
+        break;
+      }
+      case 'Redux Toolkit': {
+        await setupReactRedux();
+        break;
+      }
+      case 'No': {
+        await setupReact();
+        break;
+      }
+      default:
+        break;
+    }
 
-    //   case 'No': {
-    //     reactCommand =
-    //       programmingLanguage === 'Typescript'
-    //         ? `npx create-react-app ${projectName} --template typescript`
-    //         : `npx create-react-app ${projectName}`;
-    //     break;
-    //   }
-    //   default:
-    //     break;
-    // }
+    if (includeDocumentation) {
+      cli.action.start('Including Documentation.. ');
+      const cmd = 'npm install -save-dev react-doc-generator';
+      await executeShellCommand(cmd, insideProjectPath);
+      cli.action.stop();
+    }
 
-    // /* Run create-react-app with config */
-    // await executeShellCommand(reactCommand, projectPath);
+    //  --- HELPER METHODS ---
+    const initiateHooks = async (configLintStaged: string): Promise<void> => {
+      cli.action.start('Setting up hooks.. ');
+      const cmd = `npm install --save-dev lint-staged husky@4.3.8`;
+      await executeShellCommand(cmd, insideProjectPath);
+      await editJsonFile(
+        `${insideProjectPath}/package.json`,
+        ['husky', configLintStaged],
+        ['husky', 'lint-staged']
+      );
+      cli.action.stop();
+    };
 
-    // if (installReduxCommand !== '') {
-    //   cli.action.start('Installing redux.. ');
-    //   await executeShellCommand(installReduxCommand, insideProjectPath);
-    //   cli.action.stop();
-    // }
-    //  if (includeStyledComponents) {
-    //   cli.action.start('Installing StyledComponents.. ');
-    //   const cmd = `npm install --save styled-components@${styledComponentsVersion}`;
-    //   await executeShellCommand(cmd, insideProjectPath);
-    //   cli.action.stop();
-    // }
+    const setupPrettier = async (): Promise<void> => {
+      cli.action.start('Setting up prettier.. ');
+      const npmCmd = 'npm install --save-dev prettier';
+      await executeShellCommand(npmCmd, insideProjectPath);
+      await copyProjectConfigFile(
+        '.prettierrc.yaml',
+        `${insideProjectPath}/.prettierrc.yaml`
+      );
+      await copyProjectConfigFile(
+        '.prettierignore',
+        `${insideProjectPath}/.prettierignore`
+      );
 
-    // if (includeDocumentation) {
-    //   cli.action.start('Including Documentation.. ');
-    //   const cmd = 'npm install -save-dev react-doc-generator';
-    //   await executeShellCommand(cmd, insideProjectPath);
-    //   cli.action.stop();
-    // }
+      if (includeGitHooks) {
+        if (includeLinting) {
+          await initiateHooks('lint-staged-eslint');
+        } else {
+          await initiateHooks('lint-staged-prettier');
+        }
+      }
+      cli.action.stop();
+    };
 
-    // --- HELPER METHODS ---
-    //   const initiateHooks = async (configLintStaged: string): Promise<void> => {
-    //     cli.action.start('Setting up hooks.. ');
-    //     const cmd = `npm install --save-dev lint-staged husky@4.3.8`;
-    //     await executeShellCommand(cmd, insideProjectPath);
-    //     await editJsonFile(
-    //       `${insideProjectPath}/package.json`,
-    //       ['husky', configLintStaged],
-    //       ['husky', 'lint-staged']
-    //     );
-    //     cli.action.stop();
-    //   };
+    const setupEslint = async (cmd: string): Promise<void> => {
+      cli.action.start('Setting up eslint..');
+      await executeShellCommand(cmd, insideProjectPath);
+      programmingLanguage === 'Typescript' &&
+        (await executeShellCommand(
+          'npm install --save-dev eslint-plugin-react eslint-config-airbnb-typescript',
+          insideProjectPath
+        ));
 
-    //   const setupPrettier = async (): Promise<void> => {
-    //     cli.action.start('Setting up prettier.. ');
-    //     const npmCmd = 'npm install --save-dev prettier';
-    //     await executeShellCommand(npmCmd, insideProjectPath);
-    //     await copyProjectConfigFile(
-    //       '.prettierrc.yaml',
-    //       `${insideProjectPath}/.prettierrc.yaml`
-    //     );
-    //     await copyProjectConfigFile(
-    //       '.prettierignore',
-    //       `${insideProjectPath}/.prettierignore`
-    //     );
+      const alteredLintingStyle =
+        lintingStyle === 'airbnb' && programmingLanguage === 'Typescript'
+          ? 'airbnb-typescript'
+          : lintingStyle;
 
-    //     if (includeGitHooks) {
-    //       if (includeLinting) {
-    //         await initiateHooks('lint-staged-eslint');
-    //       } else {
-    //         await initiateHooks('lint-staged-prettier');
-    //       }
-    //     }
-    //     cli.action.stop();
-    //   };
+      if (includeFormatting) {
+        await setupPrettier();
+        await generateEslintConfig(
+          `${insideProjectPath}/.eslintrc.yaml`,
+          `eslintTemplate${langString}.yaml`,
+          true,
+          alteredLintingStyle
+        );
+      } else {
+        await generateEslintConfig(
+          `${insideProjectPath}/.eslintrc.yaml`,
+          `eslintTemplate${langString}.yaml`,
+          false,
+          alteredLintingStyle
+        );
+      }
+      cli.action.stop();
+    };
 
-    //   const setupEslint = async (cmd: string): Promise<void> => {
-    //     cli.action.start('Setting up eslint..');
-    //     await executeShellCommand(cmd, insideProjectPath);
-    //     programmingLanguage === 'Typescript' &&
-    //       (await executeShellCommand(
-    //         'npm install --save-dev eslint-plugin-react eslint-config-airbnb-typescript',
-    //         insideProjectPath
-    //       ));
+    // --- SETUP METHODS ---
 
-    //     const alteredLintingStyle =
-    //       lintingStyle === 'airbnb' && programmingLanguage === 'Typescript'
-    //         ? 'airbnb-typescript'
-    //         : lintingStyle;
+    const standardSetup = async (): Promise<void> => {
+      const cmd = includeFormatting
+        ? 'npm install --save-dev eslint-plugin-prettier prettier eslint-config-prettier eslint-config-standard eslint-plugin-node eslint-plugin-promise'
+        : 'npm install --save-dev eslint-config-standard eslint-plugin-node eslint-plugin-promise';
+      await setupEslint(cmd);
+    };
 
-    //     if (includeFormatting) {
-    //       await setupPrettier();
-    //       await generateEslintConfig(
-    //         `${insideProjectPath}/.eslintrc.yaml`,
-    //         `eslintTemplate${langString}.yaml`,
-    //         true,
-    //         alteredLintingStyle
-    //       );
-    //     } else {
-    //       await generateEslintConfig(
-    //         `${insideProjectPath}/.eslintrc.yaml`,
-    //         `eslintTemplate${langString}.yaml`,
-    //         false,
-    //         alteredLintingStyle
-    //       );
-    //     }
-    //     cli.action.stop();
-    //   };
+    const airbnbSetup = async (): Promise<void> => {
+      const cmd = includeFormatting
+        ? 'npm install --save-dev eslint-plugin-prettier prettier eslint-config-prettier eslint-config-airbnb'
+        : `npm install --save-dev eslint-config-airbnb`;
+      await setupEslint(cmd);
+    };
 
-    //   // --- SETUP METHODS ---
+    const googleSetup = async (): Promise<void> => {
+      const cmd = includeFormatting
+        ? 'npm install --save-dev eslint-plugin-prettier prettier eslint-config-prettier eslint-config-google'
+        : 'npm install --save-dev eslint-config-google';
+      await setupEslint(cmd);
+    };
 
-    //   const standardSetup = async (): Promise<void> => {
-    //     const cmd = includeFormatting
-    //       ? 'npm install --save-dev eslint-plugin-prettier prettier eslint-config-prettier eslint-config-standard eslint-plugin-node eslint-plugin-promise'
-    //       : 'npm install --save-dev eslint-config-standard eslint-plugin-node eslint-plugin-promise';
-    //     await setupEslint(cmd);
-    //   };
+    const lintingStyleDict: { [key: string]: () => Promise<void> } = {
+      standard: standardSetup,
+      airbnb: airbnbSetup,
+      google: googleSetup,
+    };
 
-    //   const airbnbSetup = async (): Promise<void> => {
-    //     const cmd = includeFormatting
-    //       ? 'npm install --save-dev eslint-plugin-prettier prettier eslint-config-prettier eslint-config-airbnb'
-    //       : `npm install --save-dev eslint-config-airbnb`;
-    //     await setupEslint(cmd);
-    //   };
-
-    //   const googleSetup = async (): Promise<void> => {
-    //     const cmd = includeFormatting
-    //       ? 'npm install --save-dev eslint-plugin-prettier prettier eslint-config-prettier eslint-config-google'
-    //       : 'npm install --save-dev eslint-config-google';
-    //     await setupEslint(cmd);
-    //   };
-
-    //   const lintingStyleDict: { [key: string]: () => Promise<void> } = {
-    //     standard: standardSetup,
-    //     airbnb: airbnbSetup,
-    //     google: googleSetup,
-    //   };
-
-    //   if (includeLinting) {
-    //     await lintingStyleDict[lintingStyle]();
-    //   } else if (includeFormatting) {
-    //     await setupPrettier();
-    //   }
+    if (includeLinting) {
+      await lintingStyleDict[lintingStyle]();
+    } else if (includeFormatting) {
+      await setupPrettier();
+    }
   };
 }
