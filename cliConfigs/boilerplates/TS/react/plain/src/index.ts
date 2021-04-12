bacd9a2ac13dbbbd7bf26528592a42148b0fd4ac
Sa_ah <sa8623ah-s@student.lu.se>
2021-04-12 15:38:31 +0200

    added more files to TS configs

--- cliConfigs/boilerplates/TS/react/plain/src/index.ts
+++ cliConfigs/boilerplates/TS/react/plain/src/index.ts
@@ -1,14 +1,13 @@
-body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
-  }
+
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import './index.css';
+  import App from './App';
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
   
